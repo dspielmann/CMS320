387c317a809afb0ec8b109f4d2eedf7ca5f5870c
dspielmann <diego.spielmann@gmail.com>
2025-10-13 19:59:26 -0400

    class

---
+++ dxball/Assets/Scripts/playerMove.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+public class move : MonoBehaviour
+{
+Rigidbody2D pad;
+Vector2 initial;
+public float displacement;
+    // Start is called once before the first execution of Update after
+    public Animator animator;
+
+
+    void Start()
+{
+pad = GetComponent<Rigidbody2D>();
+initial = pad.transform.localPosition;
+}
+// Update is called once per frame
+void Update()
+
+{
+        if ((Input.GetKey(KeyCode.RightArrow)))
+        {
+            animator.SetBool("isRunning", true);
+            if (initial.x <= 9.75)
+                initial.x = initial.x + displacement;
+        }
+        else if ((Input.GetKey(KeyCode.LeftArrow)))
+        {
+            if (initial.x > -9.75)
+                initial.x = initial.x - displacement;
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+pad.MovePosition(initial);
+}
+}
